490cac9229703952a71bda71a05d11ec73f06769
idkteh <124367770+idkteh@users.noreply.github.com>
2023-10-16 17:33:44 +0100

    ground check and jumping

--- 2d_game_engine/Assets/player_movement.cs
+++ 2d_game_engine/Assets/player_movement.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class player_movement : MonoBehaviour
 {
     public float speed = 1.5f;
     public float jump_height = 2f;
+    public LayerMask ground_layer;
+    public Transform check_for_ground;
+    public float check_for_ground_rad = 0.2f;
+    private bool grounded;
 
     public Rigidbody2D body; //creating
-                             //
+                             
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +23,15 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        
+       grounded = Physics2D.OverlapCircle(check_for_ground.position, check_for_ground_rad,ground_layer); //checks if character is on ground, uses overlap circle on character
+       
         float horizontalInput = Input.GetAxisRaw("Horizontal"); // collects input from left/right arrow or a/d
         body.velocity = new Vector2 (horizontalInput*speed,body.velocity.y ); // setting the velocity, whatever the input is * speed, not changing the y axis
-   
+        if (Input.GetKey(KeyCode.Space)&&grounded) //checks if we use space
+        {
+            Jump();
+        }
    }
 
     private void Jump()
