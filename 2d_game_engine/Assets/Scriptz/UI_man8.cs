44181f306c138289847219d5e1541135608a0791
idkteh <124367770+idkteh@users.noreply.github.com>
2023-10-23 14:57:59 +0100

    ui manager script added (empty)

--- 2d_game_engine/Assets/Scriptz/UI_man8.cs
+++ 2d_game_engine/Assets/Scriptz/UI_man8.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_man8 : MonoBehaviour
 {
+    public Image keycon; //key icon
+    public game_man7 manager; 
    
     void Start()
     {
